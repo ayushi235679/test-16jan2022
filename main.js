673e24a2177f7697000958a04147c2ab6a5b805e
ayushi235679 <85889817+ayushi235679@users.noreply.github.com>
2022-01-15 03:28:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,36 @@
 function product(){
     window.location="product.html";
 }
-$('.like-btn').on('click', function() {
-    $(this).toggleClass('is-active');
- });
- $('.minus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value >= 1) {
-        value = value - 1;
-    } else {
-        value = 0;
+
+//you have to design the UI only, below code is additional.
+
+/*function plus()
+{
+let value = parseInt(document.getElementById('quant').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value<10){
+        value++;
+            document.getElementById('quant').value = value;
+            let sum=parseInt(549*value);
+            console.log(sum);
+            document.getElementById('total-price').innerHTML = sum;   
     }
- 
-  $input.val(value);
- 
-});
- 
-$('.plus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value <= 1) {
-        value = value + 1;
-    } 
-    else {
-        value =100;
+}
+function minus()
+{
+    let value = parseInt(document.getElementById('quant').value, 10);
+    let price=parseInt(document.getElementById('total-price'));
+    value = isNaN(value) ? 0 : value;
+    if(value>=1){
+        value--;
+            document.getElementById('quant').value = value;
+            for(i=value;i>=0;i--){
+                
+                let sum=parseInt(price-549);
+            console.log(sum);
+            document.getElementById('total-price').innerHTML = sum; 
+            }
     }
- 
-    $input.val(value);
-});
+
+}*/
+
